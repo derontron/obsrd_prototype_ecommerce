fdfa4f2ac4769dbd7682ed9a82de15436ebda8ca
obsrd <deron@obsrd.com>
2017-03-02 09:26:41 -0800

    Created basic view, edit url and added debug statement in settings

--- obsrd/profiles/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class profile(models.Model):
-    name = models.CarField(max_length=120)
-
-    def __unicode__(self):
-        return self.name
